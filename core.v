ff01a1409ad535565b7f9e29e8fcda8c00936add
HATRON007 <hemanthstudies2006@gmail.com>
2026-01-23 00:22:53 +0530

    pipelined code

--- core.v
+++ core.v
@@ -7,71 +7,77 @@ module core #(
     parameter frc_width = 12,
     localparam w = 1 + int_width + frc_width,
     
-    localparam signed [w-1 : 0] a = 16'd2867
 )(
     input  wire clk,
     input  wire rst,
     input  wire signed [w-1 : 0] i,
-    output reg  signed [w-1 : 0] v,
+    input  wire signed [w-1 : 0] v,
+    input  wire signed [w-1 : 0] w_in,
+    output reg  signed [w-1 : 0] v_out,
     output reg  signed [w-1 : 0] w_out
 );
-
+    reg signed [w-1 : 0] z1_v, z1_posV, z1_negV, z1_w, z1_y1, z1_i;
     wire signed [w-1 : 0] pow2_pos, pow2_neg;
-    wire signed [19 : 0] z1, z2, z5;
-    wire signed [19 : 0] v_ext, w_ext, i_ext, diff_ext;
-    wire signed [w-1 : 0] z3, z4, z6, z7;
-    wire signed [19:0] z5_raw;
+    wire signed [19 : 0] diff_ext, y2, y4;
 
-    localparam signed [19:0] THRESHOLD = 20'd175;
 
-    assign v_ext = {{4{v[w-1]}}, v};
-    assign w_ext = {{4{w_out[w-1]}}, w_out};
-    assign i_ext = {{4{i[w-1]}}, i};
+    localparam signed [w-1 : 0] a = 16'd2867;
+    localparam signed [19 : 0] THRESHOLD = 20'd175;
     
     pow_2_function #(
-        .w(w), 
+        .w(w),
         .int_width(int_width), 
         .frc_width(frc_width)
-    ) positive_v (.x(v), .y(pow2_pos));
+    ) positve_v (.x(v), .y(pow2_pos));
 
     pow_2_function #(
-        .w(w), 
-        .int_width(int_width), 
+        .w(w),
+        .int_width(int_width),
         .frc_width(frc_width)
     ) negative_v (.x(-v), .y(pow2_neg));
 
-    // --- COMBINATIONAL LOGIC ---
-    
-    // 1. Calculate Non-Linear Term: 3 * (2^-v - 2^v)
-    assign diff_ext = {{4{pow2_neg[w-1]}} , pow2_neg} - {{4{pow2_pos[w-1]}}, pow2_pos};
-    assign z1 = (diff_ext <<< 1) + diff_ext;
-
-    // 2. Calculate Linear Term: 5 * v
-    assign z2 = (v_ext <<< 2) + v_ext;
-
-    // 3. Calculate Recovery Term: (dt/tau) * (v + a - 0.5w)
-    assign z3 = v + a - (w_out >>> 1);
-    assign z4 = (z3 + 128) >>> total_shift; // Includes rounding (+128)
-
-    // 4. Calculate Force: 5v + g(v) - w + I
-    assign z5_raw = z1 + z2 - w_ext + i_ext;
-    
-    // 5. Apply Deadzone (Stability Logic)
-    assign z5 = (z5_raw > -THRESHOLD && z5_raw < THRESHOLD) ? 20'd0 : z5_raw;
-
-    // 6. Calculate Next State (Euler Integration)
-    assign z6 = w_out + z4; 
-    assign z7 = v + $signed((z5 + 64) >>> time_shift); // Includes rounding (+64)
-
-    always @(posedge clk or posedge rst) begin
-      if (rst) begin
-        v <= 16'hECE1;     // -1.19
-        w_out <= 16'hF600; // -0.625
-      end
-      else begin
-        v <= z7;
-        w_out <= z6;
-      end 
+    always @(posedge clk) begin
+        z1_v <= v;
+        z1_w <= w_in;
+        z1_i <= i;
+        z1_y1 <= v + a - (w_in >>> 1);
+        z1_posV <= pow2_pos;
+        z1_negV <= pow2_neg;
     end
     
+    reg signed [19 : 0]z2_5v, z2_3pow;
+    reg signed [w-1 : 0] z2_v, z2_i, z2_w, z2_y3;
+
+    assign diff_ext = {{4{z1_negV[w-1]}}, z1_negV} - {{4{z1_posV[w-1]}}, z1_posV};
+    assign y2 = (({{4{z1_v[w-1]}}, z1_v}) <<< 2) + ({{4{z1_v[w-1]}}, z1_v});
+
+    always @(posedge clk) begin 
+        z2_v <= z1_v;
+        z2_i <= z1_i;
+        z2_5v <= y2;
+        z2_w <= z1_w;
+        z2_3pow <= (diff_ext <<< 1) + diff_ext;
+        z2_y3 <= ((z1_y1 + 128) >>> total_shift);
+    end
+
+    assign y4 = z2_3pow + z2_5v - ({{4{z2_w[w-1]}}, z2_w}) + {{4{z2_i[w-1]}}, z2_i};
+    reg signed [w-1 : 0] z3_v, z3_w;
+    reg signed [19 : 0] z3_dv;
+    always @(posedge clk) begin
+        z3_v <= z2_v;
+        z3_dv <= (y4 > -THRESHOLD && y4 < THRESHOLD) ? 20'd0 : y4;
+        z3_w <= z2_y3 + z2_w;
+    end
+
+    always @(posedge clk or posedge rst) begin
+        if (rst) begin
+            v_out <= 16'hECE1;
+            w_out <= 16'hF600;
+        end
+        else begin
+            v_out <= z3_v + $signed((z3_dv + 64) >>> time_shift);
+            w_out <= z3_w;
+        end
+    end
+
 endmodule
